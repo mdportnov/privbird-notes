ed3701d9ceaf2f8e1987541e25c39334eba59e10
Krasnopolsky Ivan <i1.krasnopolskiy@gmail.com>
2022-11-03 00:33:01 +0300

    Prettify project package

--- backend/privbird/celery.py
+++ backend/privbird/celery.py
@@ -1,11 +1,12 @@
+import os
+
 from celery import Celery
 from celery.schedules import crontab
-import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'privbird.settings')
 
 app = Celery('privbird')
-app.config_from_object('django.conf:settings', namespace = 'CELERY')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
--- backend/privbird/urls.py
+++ backend/privbird/urls.py
@@ -1,18 +1,3 @@
-"""privbird URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name = 'home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name = 'home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path, re_path
 
@@ -22,17 +7,17 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
     Info(
-        title = 'Note API',
-        default_version = 'v1',
-        terms_of_service = 'https://www.google.com/policies/terms/',
+        title='Note API',
+        default_version='v1',
+        terms_of_service='https://www.google.com/policies/terms/',
     ),
-    public = True,
-    permission_classes = (permissions.AllowAny,),
+    public=True,
+    permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
     path('', include('secnote.urls')),
     path('admin/', admin.site.urls),
-    re_path(r'^api/$', schema_view.with_ui('swagger', cache_timeout = 0), name = 'schema-swagger-ui'),
-    re_path(r'^docs/$', schema_view.with_ui('redoc', cache_timeout = 0), name = 'schema-redoc'),
+    re_path(r'^api/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^docs/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
