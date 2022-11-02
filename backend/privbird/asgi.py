9ff16b4804fd8020f7a9b0d57a8b16be8e2539a5
Krasnopolsky Ivan <i1.krasnopolskiy@gmail.com>
2022-11-03 00:30:03 +0300

    Rename project

--- backend/privbird/asgi.py
+++ backend/privbird/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for onetimelinks project.
+ASGI config for privbird project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -10,6 +10,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onetimelinks.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'privbird.settings')
 
 application = get_asgi_application()
