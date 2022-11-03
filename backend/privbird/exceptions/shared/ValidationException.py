62796cf2f5f1c358c82584921f6b2c6ea496afcc
Krasnopolsky Ivan <i1.krasnopolskiy@gmail.com>
2022-11-03 08:52:36 +0300

    Connect with mariadb

--- backend/privbird/exceptions/shared/ValidationException.py
+++ backend/privbird/exceptions/shared/ValidationException.py
@@ -8,7 +8,7 @@ from privbird.exceptions.shared.ApiException import ApiException
 class ValidationException(ApiException):
 
     def __init__(self, error: ValidationError):
-        self.ru = "Некорректные данные"
-        self.en = "Invalid data"
+        self.ru = 'Некорректные данные'
+        self.en = 'Invalid data'
         self.status_code = error.status_code
         self.errors = json.dumps(error.detail)
--- backend/privbird/urls.py
+++ backend/privbird/urls.py
@@ -5,8 +5,8 @@ import feedbacks.views
 import notes.views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("feedback/", feedbacks.views.PostFeedbackView.as_view()),
-    path("notes/", notes.views.PostNoteView.as_view()),
-    path("notes/<slug:slug>/", notes.views.NoteView.as_view())
+    path('admin/', admin.site.urls),
+    path('feedback/', feedbacks.views.PostFeedbackView.as_view()),
+    path('notes/', notes.views.PostNoteView.as_view()),
+    path('notes/<slug:slug>/', notes.views.NoteView.as_view())
 ]
