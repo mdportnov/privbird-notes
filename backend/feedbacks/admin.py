0657400190614c1cb17236754a26827b357b6f26
Krasnopolsky Ivan <i1.krasnopolskiy@gmail.com>
2022-11-04 12:43:42 +0300

    Add administration pages

--- backend/feedbacks/admin.py
+++ backend/feedbacks/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from feedbacks.models import Feedback
+from feedbacks.utils.constants import Constants
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created_at', 'email', 'feedback_preview')
+    list_filter = ('created_at',)
+    search_fields = ('email', 'feedback')
+
+    @admin.display(description='feedback')
+    def feedback_preview(self, model: Feedback):
+        preview = model.feedback
+        if len(preview) > Constants.FEEDBACK_PREVIEW_LENGTH:
+            preview = preview[:Constants.FEEDBACK_PREVIEW_LENGTH] + '...'
+        return preview
--- backend/notes/admin.py
+++ backend/notes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from notes.models import Note
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'slug', 'expires', 'network')
+    list_filter = ('expires', 'network')
+    search_fields = ('slug',)
