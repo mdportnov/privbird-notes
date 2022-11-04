b1dce7c45fa950853fb5e50706feb6db1b46b394
Krasnopolsky Ivan <i1.krasnopolskiy@gmail.com>
2022-11-04 12:44:15 +0300

    Remove useless code

--- backend/notes/views.py
+++ backend/notes/views.py
@@ -2,7 +2,6 @@ from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.exceptions import ValidationError
 from rest_framework.request import Request
-from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from notes.dto.request.CreateNoteRequest import CreateNoteRequest
