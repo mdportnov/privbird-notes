c99c8a94794d46ee3076fd6fdf65cfdf8a94aad5
Krasnopolsky Ivan <i1.krasnopolskiy@gmail.com>
2022-11-21 15:16:48 +0300

    Change delimiter

--- backend/notes/urls.py
+++ backend/notes/urls.py
@@ -4,6 +4,6 @@ from notes import views
 
 urlpatterns = [
     path('', views.CreateNoteView.as_view()),
+    path('<slug:slug>-<slug:key>/', views.NoteKeyView.as_view()),
     path('<slug:slug>/', views.NotePasswordView.as_view()),
-    path('<slug:slug>/<slug:key>/', views.NoteKeyView.as_view())
 ]
