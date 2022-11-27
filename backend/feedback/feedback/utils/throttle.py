a2083565b26a9640fc04bef25020b44707524f0a
Krasnopolsky Ivan <i1.krasnopolskiy@gmail.com>
2022-11-27 12:41:58 +0300

    Update backend structure

---
+++ backend/feedback/feedback/utils/throttle.py
@@ -0,0 +1,11 @@
+from django.core.cache import cache
+from rest_framework.request import Request
+from rest_framework.throttling import SimpleRateThrottle
+
+
+class RateThrottle(SimpleRateThrottle):
+    rate = '100/h'
+
+    def get_cache_key(self, request: Request, view):
+        addr = request.META['REMOTE_ADDR']
+        return cache.get_or_set(addr, 0, 0) + 1
