743f06c0dcba15385ad82b5d06b427e7234f26c2
Krasnopolsky Ivan <i1.krasnopolskiy@gmail.com>
2022-11-30 23:44:19 +0300

    Update tests

--- backend/feedback/feedback/utils/throttle.py
+++ backend/feedback/feedback/utils/throttle.py
@@ -2,6 +2,8 @@ from django.core.cache import cache
 from rest_framework.request import Request
 from rest_framework.throttling import SimpleRateThrottle
 
+from feedback import settings
+
 
 class RateThrottle(SimpleRateThrottle):
     rate = '100/h'
@@ -9,3 +11,7 @@ class RateThrottle(SimpleRateThrottle):
     def get_cache_key(self, request: Request, view):
         addr = request.META['REMOTE_ADDR']
         return cache.get_or_set(addr, 0, 0) + 1
+
+    def allow_request(self, request, view):
+        allowed = super().allow_request(request, view)
+        return allowed or settings.TEST
--- backend/feedback/feedbacks/tests.py
+++ backend/feedback/feedbacks/tests.py
@@ -5,6 +5,8 @@ from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.test import APIClient
 
+URL = '/feedback/api'
+
 
 class FeedbackCreateTestCase(TestCase):
     def test_create(self):
@@ -13,5 +15,5 @@ class FeedbackCreateTestCase(TestCase):
             'feedback': token_hex(),
             'user': 'admin@mail.com'
         }
-        response: JsonResponse = client.post('/api/feedbacks/', data, format='json')
+        response: JsonResponse = client.post(f'{URL}/feedbacks/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
