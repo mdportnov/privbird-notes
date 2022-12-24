6bddb10046e66c9ee0682ec0814ddd51abd1c83d
Krasnopolsky Ivan <i1.krasnopolskiy@gmail.com>
2022-12-24 23:50:37 +0300

    Fix celery consumer

--- backend/privnote/privnote/settings.py
+++ backend/privnote/privnote/settings.py
@@ -172,6 +172,10 @@ CELERY_BROKER_URL = 'amqp://' + RABBITMQ_LOCATION
 CELERY_RESULT_BACKEND = 'rpc://' + RABBITMQ_LOCATION
 CELERY_DEFAULT_QUEUE = getenv('CELERY_DEFAULT_QUEUE', 'HTTPS')
 CELERY_ALLOWED_QUEUES = {'HTTPS', 'TOR', 'I2P'}
+CELERY_ROUTES = {
+    'notes.tasks.*': {'queue': CELERY_DEFAULT_QUEUE},
+}
+
 
 # Email server configuration
 EMAIL_BACKEND = getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
