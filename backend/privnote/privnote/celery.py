bcd8b41354bb4f84995a161d4771c95f467e476d
Krasnopolsky Ivan <i1.krasnopolskiy@gmail.com>
2022-12-24 15:05:08 +0300

    Fix queue

--- backend/privnote/privnote/celery.py
+++ backend/privnote/privnote/celery.py
@@ -16,6 +16,10 @@ app.conf.task_default_exchange = 'topic_notes'
 app.conf.task_queues = tuple(Queue(queue, routing_key=queue) for queue in settings.CELERY_ALLOWED_QUEUES)
 app.conf.task_default_queue = settings.CELERY_DEFAULT_QUEUE
 
+app.conf.task_routes = {
+    'notes.tasks.note_save': {'queue': settings.CELERY_DEFAULT_QUEUE}
+}
+
 app.conf.beat_schedule = {
     'delete-notes': {
         'task': 'notes.tasks.note_delete_expired',
